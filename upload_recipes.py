72c4b94648648d5f50737d9dbf6ac1c15417539d
gaoxin-jfrogchina <31504568+xingao0803@users.noreply.github.com>
2018-12-12 18:34:20 +0800

    Update upload_recipes.py

--- upload_recipes.py
+++ upload_recipes.py
@@ -1,4 +1,6 @@
 import os
+import sys
 
 for lib in ["A", "B", "C", "D", "F", "G"]:
-    os.system('cd recipes/%s && conan create . "LIB_%s/1.0@jfrog/stable" && conan upload "LIB_%s/1.0@jfrog/stable" -r arti-pro -c' % (lib, lib, lib))
+    os.system('cd recipes/%s && conan create -pr ../../profiles/32bits . "LIB_%s/1.0@jfrog/stable" && conan upload "LIB_%s/1.0@jfrog/stable" -r %s -c --all' % (lib, lib, lib, sys.argv[1]))
+    os.system('cd recipes/%s && conan create -pr ../../profiles/64bits . "LIB_%s/1.0@jfrog/stable" && conan upload "LIB_%s/1.0@jfrog/stable" -r %s -c --all' % (lib, lib, lib, sys.argv[1]))
