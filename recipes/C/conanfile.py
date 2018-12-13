c282f724bb9388950d33ec17367f36dec2d2299b
gaoxin-jfrogchina <31504568+xingao0803@users.noreply.github.com>
2018-12-13 11:30:00 +0800

    Update conanfile.py

--- recipes/C/conanfile.py
+++ recipes/C/conanfile.py
@@ -6,7 +6,7 @@ class CConan(ConanFile):
     version = "1.0"
     license = "MIT"
     url = "https://github.com/xingao0803/demo-multi-builds"
-    settings = "os", "compiler", "build_type", "arch"
+    settings = "os", "compiler", "build_type", "arch", "arch_build"
 
     def build(self):
         self.output.warn("Building library...")
